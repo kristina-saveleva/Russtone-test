b3b0143e25189353189fff37d45af98bf5b91e35
Kristina <tina.-.kristina@mail.ru>
2023-01-26 03:08:14 +0300

    first test

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.offer').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
