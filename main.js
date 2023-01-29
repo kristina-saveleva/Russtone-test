8651da8588381413c2a91dc99a20eefd140c3be7
Kristina <tina.-.kristina@mail.ru>
2023-01-30 02:09:57 +0300

    added adaptive

--- main.js
+++ main.js
@@ -8,3 +8,22 @@ $(function () {
         cssEase: 'linear'
     });
 });
+
+$('.menu').click (function(){
+    $(this).toggleClass('open');
+});
+
+$(function setEqualWidth() {
+	if ($(window).width() <= 768) {
+        $(".menu").prepend($(".header__menu-list"));
+		$(".menu").prepend($(".header__menu-catalog-list"));
+		$(".menu").prepend($(".header__search"));
+	}
+	$(window).on('load', setEqualWidth);
+	$(window).on('resize', function() {
+		clearTimeout(window.resizedFinished);
+		window.resizedFinished = setTimeout(function() {
+			setEqualWidth();
+		}, 500);
+	})
+});
